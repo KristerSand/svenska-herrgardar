28a63139987a607cf74d29eba9c1073f7108d56e
Krister Sandh <krister77@gmail.com>
2019-09-01 12:11:01 +0200

    New migration file

---
+++ plugins/sandit/mansion/updates/builder_table_update_sandit_mansion_import.php
@@ -0,0 +1,23 @@
+<?php namespace Sandit\Mansion\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateSanditMansionImport extends Migration
+{
+    public function up()
+    {
+        Schema::table('sandit_mansion_import', function($table)
+        {
+            $table->string('file_name')->after('import_type')->nullable();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('sandit_mansion_import', function($table)
+        {
+            $table->dropColumn('file_name');
+        });
+    }
+}
