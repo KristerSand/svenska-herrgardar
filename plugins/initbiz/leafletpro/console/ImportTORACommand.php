6795dbf50b3fcabc59b8fabed0bea2e762b7047c
Gustav Boström <gustav.bostrom@crisp.se>
2019-12-29 23:23:45 +0100

    skeleton import command

---
+++ plugins/initbiz/leafletpro/console/ImportTORACommand.php
@@ -0,0 +1,46 @@
+<?php namespace Initbiz\LeafletPro\Console;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class ImportTORACommand extends Command
+{
+    /**
+     * @var string The console command name.
+     */
+    protected $name = 'acme:importtoraids';
+
+    /**
+     * @var string The console command description.
+     */
+    protected $description = 'Does something cool.';
+
+    /**
+     * Execute the console command.
+     * @return void
+     */
+    public function handle()
+    {
+        $this->output->writeln('Hello world!');
+    }
+
+    /**
+     * Get the console command arguments.
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [];
+    }
+
+    /**
+     * Get the console command options.
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [];
+    }
+
+}
