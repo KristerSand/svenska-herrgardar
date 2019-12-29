ccb287915f5eac3fb88fe90dcbfe593d3a242851
Gustav Boström <gustav.bostrom@crisp.se>
2019-12-29 16:42:48 +0100

    downloading from url instead of file

--- plugins/initbiz/leafletpro/get_tora_json.php
+++ plugins/initbiz/leafletpro/get_tora_json.php
@@ -2,7 +2,7 @@
 
 $url_net = 'https://tora.entryscape.net/store/search?type=solr&query=rdfType:https%5C%3A%2F%2Fdata.riksarkivet.se%2Ftora%2Fschema%2FHistoricalSettlementUnit+AND+public:true+AND+(metadata.predicate.uri.abcbc6c6:https%5C%3A%2F%2Fdata.riksarkivet.se%2Ftora%2Ftorapartner%2Fherr)+AND+(title:***+OR+description:*+OR+tag.literal:***)&facetFields=metadata.predicate.uri.abcbc6c6,metadata.predicate.uri.023ae722,metadata.predicate.uri.957f77a7&request.preventCache=7'; // path to your JSON file
 $file_url = "tora_all_manors.json";
-$data = file_get_contents($file_url); // put the contents of the file into a variable
+$data = file_get_contents($url_net); // put the contents of the file into a variable
 $characters = json_decode($data); // decode the JSON feed
 
 echo "tora_id,lat,long\n";
