95c97abec9d044d7337f8bd355d640d8ca3a3525
Gustav Boström <gustav.bostrom@crisp.se>
2020-07-08 20:48:01 +0200

    Check for suecia images

--- plugins/sandit/mansion/components/SearchResult.php
+++ plugins/sandit/mansion/components/SearchResult.php
@@ -61,14 +61,36 @@ class SearchResult extends ComponentBase
         $this->result = $result;
     }
 
+
+    private function hasSueciaImages($toraid) {
+        $sueciaurl = "https://tora.entryscape.net/store/search?type=solr&query=context:https%5C%3A%2F%2Ftora.entryscape.net%2Fstore%2F11+AND+rdfType:http%5C%3A%2F%2Fschema.org%2FImageObject+AND+metadata.predicate.uri.ac99d93f:https%5C%3A%2F%2Fdata.riksarkivet.se%2Ftora%2F" . $toraid . "+AND+metadata.predicate.literal.b5d28d0a:image%2Fjpeg&request.preventCache=1523136574811";
+        $arrContextOptions=array(
+            "ssl"=>array(
+                    "verify_peer"=>true,
+                    "verify_peer_name"=>true,
+                     "cafile" =>"./cert.pem"
+                ),
+            );
+        $suecia_json = file_get_contents($sueciaurl, false, stream_context_create($arrContextOptions));
+        $suecia_record = json_decode($suecia_json); 
+        $nr_of_image_records = count($suecia_record->resource->children);
+        if ($nr_of_image_records > 0 ) {
+            return true;
+        } else {
+            return false;
+        }
+       
+    }
+
     public function onShowMansionData()
     {
         $gard_id = Input::get('gard_id');
         $search_repo = App::make('SearchRepositoryInterface');
         $gard_data = $search_repo->getGardar([$gard_id], 'id', ['post'])->first();
+        $suecia = $this->hasSueciaImages($gard_data->toraid);
         
         return [
-            '#gard-info-'.$gard_id => $this->renderPartial('@gard-info', ['gard_data' => $gard_data])
+            '#gard-info-'.$gard_id => $this->renderPartial('@gard-info', ['gard_data' => $gard_data, 'suecia' => $suecia])
         ];
     }
 }
